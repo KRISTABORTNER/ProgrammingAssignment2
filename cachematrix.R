ad92cb82156dd8b4bb2f144f6d69d35d828f7527
KRISTABORTNER <kristabortner@gmail.com>
2015-10-20 13:21:30 -0400

    Update cachematrix
    ## Creates matrix object that can cache its inverse
    makeCacheMatrix <- function( m = matrix() ) {
      
      ## Initialize the property
      i <- NULL
      
      ## setmatrix
      set <- function( matrix ) {
        m <<- matrix
        i <<- NULL
      }
      
      ## getmatrix
      get <- function() {
        ## Return the matrix
        m
      }
      
      ##inversematrix
      setInverse <- function(inverse) {
        i <<- inverse
      }
      
      ##getinversematrix
      getInverse <- function() {
        ## Return the inverse property
        i
      }
      
      ##returnmethods
      list(set = set, get = get,
           setInverse = setInverse,
           getInverse = getInverse)
    }
    
    
    ## Compute the inverse matrix returned by "makeCacheMatrix" above.
    v

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates matrix object that can cache its inverse
+makeCacheMatrix <- function( m = matrix() ) {
+  
+  ## Initialize the property
+  i <- NULL
+  
+  ## setmatrix
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  ## getmatrix
+  get <- function() {
+    ## Return the matrix
+    m
+  }
+  
+  ##inversematrix
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ##getinversematrix
+  getInverse <- function() {
+    ## Return the inverse property
+    i
+  }
+  
+  ##returnmethods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Compute the inverse matrix returned by "makeCacheMatrix" above.
